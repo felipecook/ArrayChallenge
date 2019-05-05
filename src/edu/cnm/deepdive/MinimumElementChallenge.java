d9c50269cbdf601a0e5ba9cfacabb260b8793f72
Felipe Cook <felipepcook@gmail.com>
2019-05-05 15:38:27 -0600

    Starting MinimumElementChallenge

---
+++ src/edu/cnm/deepdive/MinimumElementChallenge.java
@@ -0,0 +1,21 @@
+package edu.cnm.deepdive;
+
+import java.util.Scanner;
+
+public class MinimumElementChallenge {
+
+  Scanner scanner = new Scanner(System.in);
+
+  public static int[] readIntegers(int count) {
+
+  }
+
+  public static int findMin(int[] array) {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
