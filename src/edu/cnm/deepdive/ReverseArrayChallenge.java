855f0669ebc4fbd5539a3415866cbbcad49500ed
Felipe Cook <felipepcook@gmail.com>
2019-05-06 20:56:45 -0600

    Swapped initial end and beginning values

--- src/edu/cnm/deepdive/ReverseArrayChallenge.java
+++ src/edu/cnm/deepdive/ReverseArrayChallenge.java
@@ -5,13 +5,24 @@ public class ReverseArrayChallenge {
   public static void reverse(int[] array) {
 //    int[] arrayReversed = new int[array.length];
     int holderVar;
+    if (array.length <= 1) {
+      return;
+    }
+    if (array.length == 2) {
+      holderVar = array[0];
+      array[0] = array[1];
+      array[1] = holderVar;
+    }
+    holderVar = array[array.length - 1];
+    array[array.length - 1] = array[0];
+    array[0] = holderVar;
+    holderVar = array[1];
 
-
-    for (int i = 0; i < array.length; i++) {
+    for (int i = 1; i < array.length - 2; i++) {
 //      arrayReversed[i] = array[--holderVar];
-      holderVar = array[i];
       array[i] = array[i + 1];
     }
+    array[array.length -2] = holderVar;
   }
 
   public static void main(String[] args) {
