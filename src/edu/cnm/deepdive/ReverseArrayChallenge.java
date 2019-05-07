41dc2373983e383eb6dcfe8314e169a0803e4a0f
Felipe Cook <felipepcook@gmail.com>
2019-05-06 21:08:08 -0600

    Solved reverse method

--- src/edu/cnm/deepdive/ReverseArrayChallenge.java
+++ src/edu/cnm/deepdive/ReverseArrayChallenge.java
@@ -3,33 +3,55 @@ package edu.cnm.deepdive;
 public class ReverseArrayChallenge {
 
   public static void reverse(int[] array) {
-//    int[] arrayReversed = new int[array.length];
-    int holderVar;
-    if (array.length <= 1) {
-      return;
-    }
-    if (array.length == 2) {
-      holderVar = array[0];
-      array[0] = array[1];
-      array[1] = holderVar;
-    }
-    holderVar = array[array.length - 1];
-    array[array.length - 1] = array[0];
-    array[0] = holderVar;
-    holderVar = array[1];
 
-    for (int i = 1; i < array.length - 2; i++) {
-//      arrayReversed[i] = array[--holderVar];
-      array[i] = array[i + 1];
+    int maxIndex = array.length - 1;
+    int halfwayIndex = array.length / 2;
+
+    for (int i = 0; i < halfwayIndex; i++) {
+      int holder = array[i];
+      array[i] = array[maxIndex - i];
+      array[maxIndex - i] = holder;
+
     }
-    array[array.length -2] = holderVar;
+
+
+//    int holderVar;
+//    if (array.length <= 1) {
+//      return;
+//    }
+//    if (array.length == 2) {
+//      holderVar = array[0];
+//      array[0] = array[1];
+//      array[1] = holderVar;
+//    }
+//    holderVar = array[array.length - 1];
+//    array[array.length - 1] = array[0];
+//    array[0] = holderVar;
+//    holderVar = array[1];
+
+//    for (int i = 1; i < array.length - 2; i++) {
+//      arrayReversed[i] = array[--holderVar];
+//      array[i] = array[i + 1];
+//    }
+//    array[array.length -2] = holderVar;
+
+
   }
+
+
 
   public static void main(String[] args) {
     int[] intArray = {1, 3, 5, 6, 7};
-    System.out.println(intArray);
+    for (int number:
+    intArray) {
+      System.out.println(number);
+    }
     reverse(intArray);
-    System.out.println(intArray);
+    for (int number:
+        intArray) {
+      System.out.println(number);
+    }
+
   }
 
 
